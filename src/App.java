d8661f2d8a7b6d329b12b939c8f373f7cc8ee2f3
Zsomi4 <k72302872172@ktch.hu>
2022-10-26 13:45:56 +0200

    Fejlécek megírása.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
+/*
+* File: App.java
+* Author: Gubis Zsombor Dániel
+* Copyright: 2022, Gubis Zsombor Dániel
+* Group: Szoft_II_N
+* Date: 2022-10-26
+* Github: https://github.com/Zsomi4/
+* Licenc: GNU GPL
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
     }
--- src/Names.java
+++ src/Names.java
@@ -1,3 +1,13 @@
+/*
+* File: Names.java
+* Author: Gubis Zsombor Dániel
+* Copyright: 2022, Gubis Zsombor Dániel
+* Group: Szoft_II_N
+* Date: 2022-10-26
+* Github: https://github.com/Zsomi4/
+* Licenc: GNU GPL
+*/
+
 public interface Names {
     
 }
